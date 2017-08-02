442205da0167a325f7c67c86ae165d48887ac79c
zhuzhongwei <1510331524@qq.com>
2017-08-02 18:48:02 +0800

    OK

---
+++ hansen-common-service/src/main/java/com/hansen/common/constant/GradeType.java
@@ -0,0 +1,42 @@
+package com.hansen.common.constant;
+
+/**
+ * @author jay.zheng
+ * @date 2017/7/12
+ */
+public enum GradeType {
+
+    /** 一星 */
+    GRADE1(1, "一星"),
+
+    /** 二星 */
+    GRADE2(2, "二星"),
+
+    /** 三星 */
+    GRADE3(3, "三星"),
+
+    /** 四星 */
+    GRADE4(4, "四星"),
+
+    /** 五星 */
+    GRADE5(5, "五星");
+
+
+    private final Integer code;
+
+    private final String msg;
+
+    private GradeType(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+}
---
+++ hansen-common-service/src/main/java/com/hansen/common/constant/OrderType.java
@@ -0,0 +1,31 @@
+package com.hansen.common.constant;
+
+/**
+ * 订单类型枚举类 <br/>
+ * 
+ * @author zhuzh
+ * @date 2016年11月15日
+ */
+public enum OrderType {
+	PURCHASE(1, "直接购买"),
+
+	CARTPAY(2, "购物车购买");
+
+	private final Integer code;
+
+	private final String msg;
+
+	private OrderType(Integer code, String msg) {
+		this.code = code;
+		this.msg = msg;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+}
