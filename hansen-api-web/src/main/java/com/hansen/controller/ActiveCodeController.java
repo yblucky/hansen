c722ebba102f44979272dd4c8b5bf9bea6e2909e
lxl <1716473044@qq.com>
2017-08-06 20:13:09 +0800

    ok

---
+++ hansen-api-web/src/main/java/com/hansen/controller/ActiveCodeController.java
@@ -0,0 +1,95 @@
+package com.hansen.controller;
+
+import com.base.page.JsonResult;
+import com.common.Token;
+import com.common.base.TokenUtil;
+import com.common.constant.CodeType;
+import com.common.utils.numberutils.UUIDUtil;
+import com.common.utils.toolutils.ToolUtil;
+import com.hansen.service.ActiveCodeService;
+import com.hansen.vo.CodeVo;
+import com.model.ActiveCode;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/8/6.
+ */
+@Controller
+@RequestMapping("/activecode")
+public class ActiveCodeController {
+
+    @Autowired
+    private ActiveCodeService activeCodeService;
+
+    /**
+     * 生成激活码和注册码
+     * @param request
+     * @param response
+     * @param vo
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequestMapping(value = "/generateCode", method = RequestMethod.POST)
+    public JsonResult generateCode(HttpServletRequest request, HttpServletResponse response, @RequestBody CodeVo vo) throws Exception {
+
+        Token token = TokenUtil.getSessionUser(request);
+
+        if(ToolUtil.isEmpty(vo.getCodeType())){
+            return new JsonResult(1,"生成码的类型不能为空");
+        }
+
+        //创建生成码的记录
+        ActiveCode activeCode = new ActiveCode();
+        activeCode.setCode(UUIDUtil.generateUUID());
+        activeCode.setStatus(1);
+        activeCode.setOwnerId(token.getId());
+        if(vo.getCodeType().intValue() == CodeType.ACTIVATECODE.getCode().intValue()){
+            activeCode.setType(CodeType.ACTIVATECODE.getCode());
+            activeCode.setRemark(CodeType.ACTIVATECODE.getMsg());
+        }else{
+            activeCode.setType(CodeType.REGISTERCODE.getCode());
+            activeCode.setRemark(CodeType.REGISTERCODE.getMsg());
+        }
+
+        //生成码
+        activeCodeService.createWithUUID(activeCode);
+
+        return new JsonResult();
+    }
+
+
+    /**
+     * 根据用户id查询所拥有的激活码和生成码
+     * @param request
+     * @param response
+     * @param vo
+     * @return
+     * @throws Exception
+     */
+    @ResponseBody
+    @RequestMapping(value = "/getExistCode", method = RequestMethod.POST)
+    public JsonResult getExistCode(HttpServletRequest request, HttpServletResponse response, @RequestBody CodeVo vo) throws Exception {
+        Token token = TokenUtil.getSessionUser(request);
+
+        ActiveCode model = new ActiveCode();
+        model.setStatus(1);
+        model.setOwnerId(token.getId());
+        model.setType(vo.getCodeType());
+        List<ActiveCode> codeList =  activeCodeService.readAll(model);
+
+        return new JsonResult(codeList);
+    }
+
+
+}
---
+++ hansen-core/src/main/java/com/common/utils/numberutils/UUIDUtil.java
@@ -0,0 +1,68 @@
+package com.common.utils.numberutils;
+
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * UUID生成器
+ *
+ * @date 2016年12月7日
+ */
+public class UUIDUtil {
+
+	/**
+	 * 生成一个UUID
+	 * 
+	 * @return
+	 */
+	public static String getUUID() {
+		return UUID.randomUUID().toString().replaceAll("-", "").toUpperCase();
+	}
+
+	/**
+	 * 生成多个UUID
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static String[] getUUID(int number) {
+		if (number < 1) {
+			return null;
+		}
+		String[] ss = new String[number];
+		for (int i = 0; i < number; i++) {
+			ss[i] = getUUID();
+		}
+		return ss;
+	}
+
+
+	/**
+	 * 生成UUID
+	 *
+	 * @return
+	 */
+	public static String generateUUID() {
+		return UUID.randomUUID().toString().toUpperCase();
+	}
+
+	/**
+	 *随机生成8位数授权码
+	 *
+	 */
+	public static String randomUtil(){
+		Random r = new Random();
+		String code = "";
+		for (int i = 0; i < 64; ++i) {
+			int temp = r.nextInt(52);
+			char x = (char) (temp < 26 ? temp + 97 : (temp % 26) + 65);
+			code += x;
+		}
+		return code;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(generateUUID());
+	}
+
+}
