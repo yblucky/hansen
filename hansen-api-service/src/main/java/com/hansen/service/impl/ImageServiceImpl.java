ea82e12f3eeb6a590ecc988dff267a4b5480aa88
zhuzhongwei <1510331524@qq.com>
2017-08-06 14:50:59 +0800

    OK

---
+++ hansen-api-service/src/main/java/com/hansen/service/impl/ImageServiceImpl.java
@@ -0,0 +1,30 @@
+package com.hansen.service.impl;
+
+import com.base.dao.CommonDao;
+import com.base.service.impl.CommonServiceImpl;
+import com.hansen.mapper.CardGradeMapper;
+import com.hansen.mapper.ImageMapper;
+import com.hansen.service.CardGradeService;
+import com.hansen.service.ImageService;
+import com.model.CardGrade;
+import com.model.Image;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @date 2016年11月27日
+ */
+@Service
+public class ImageServiceImpl extends CommonServiceImpl<Image> implements ImageService {
+    @Autowired
+    private ImageMapper imageMapper;
+    @Override
+    protected CommonDao<Image> getDao() {
+        return imageMapper;
+    }
+
+    @Override
+    protected Class<Image> getModelClass() {
+        return Image.class;
+    }
+}
