f31315d5038e7e476b40cd5aa1648af5f2a6de97
zhuzhongwei <1510331524@qq.com>
2017-08-17 17:26:18 +0800

    OK

---
+++ hansen-api-web/src/main/java/com/hansen/vo/CodeTransferVo.java
@@ -0,0 +1,35 @@
+package com.hansen.vo;
+
+/**
+ * 激活码转赠
+ * Created by Administrator on 2018/08/17.
+ */
+public class CodeTransferVo {
+    private Integer toUid;
+    private String toId;
+    private Integer transferNo;
+
+    public Integer getToUid() {
+        return toUid;
+    }
+
+    public void setToUid(Integer toUid) {
+        this.toUid = toUid;
+    }
+
+    public String getToId() {
+        return toId;
+    }
+
+    public void setToId(String toId) {
+        this.toId = toId;
+    }
+
+    public Integer getTransferNo() {
+        return transferNo;
+    }
+
+    public void setTransferNo(Integer transferNo) {
+        this.transferNo = transferNo;
+    }
+}
---
+++ hansen-api-web/src/main/java/com/hansen/vo/TaskVo.java
@@ -0,0 +1,17 @@
+package com.hansen.vo;
+
+/**
+ * 用户点击任务
+ * Created by Administrator on 2018/08/17.
+ */
+public class TaskVo {
+    private String taskId;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+}
