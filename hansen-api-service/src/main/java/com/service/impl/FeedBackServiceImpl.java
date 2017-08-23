3e3bddeaa21e2526a0f7048b1a7eb954343928ac
zzwei <1510331524@qq.com>
2017-08-23 23:55:49 +0800

    OK

---
+++ hansen-api-service/src/main/java/com/service/impl/FeedBackServiceImpl.java
@@ -0,0 +1,30 @@
+package com.service.impl;
+
+import com.base.dao.CommonDao;
+import com.base.service.impl.CommonServiceImpl;
+import com.mapper.FeedBackMapper;
+import com.model.FeedBack;
+import com.service.FeedBackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FeedBackServiceImpl extends CommonServiceImpl<FeedBack> implements FeedBackService {
+
+    @Autowired
+    private FeedBackMapper feedBackMapper;
+
+
+    @Override
+    protected CommonDao<FeedBack> getDao() {
+        return feedBackMapper;
+    }
+
+    @Override
+    protected Class<FeedBack> getModelClass() {
+        return FeedBack.class;
+    }
+
+
+}
