f97c9b94f9d98f0efe729daf4c64f7d9b4d012e5
zhuzhongwei <1510331524@qq.com>
2017-08-23 10:53:07 +0800

    OK

--- hansen-model/src/main/java/com/model/SysRole.java
+++ hansen-model/src/main/java/com/model/SysRole.java
@@ -2,51 +2,44 @@ package com.model;
 
 import com.BaseModel;
 
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 /**
  * 角色Po类
  */
-@Table(name="sys_role")
 public class SysRole extends BaseModel {
-	/**
-	 * 主键编号
-	 */
-	@Id
-	private String id;
-	/**
-	 * 角色名
-	 */
-	private String roleName;
+    /**
+     * 主键编号
+     */
+    private String id;
+    /**
+     * 角色名
+     */
+    private String roleName;
 
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
-	}
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	/**
-	 * @return the roleName
-	 */
-	public String getRoleName() {
-		return roleName;
-	}
+    /**
+     * @return the roleName
+     */
+    public String getRoleName() {
+        return roleName;
+    }
 
-	/**
-	 * @param roleName
-	 *            the roleName to set
-	 */
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
+    /**
+     * @param roleName the roleName to set
+     */
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
