bd0c424dd9105a30318681a5481f1d33ba59a12d
zzwei <1510331524@qq.com>
2017-08-25 10:00:48 +0800

    OK

--- hansen-api-service/src/main/java/com/mapper/WalletOrderMapper.java
+++ hansen-api-service/src/main/java/com/mapper/WalletOrderMapper.java
@@ -1,11 +1,15 @@
 package com.mapper;
 
 import com.base.dao.CommonDao;
+import com.base.page.Page;
 import com.model.WalletOrder;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WalletOrderMapper extends CommonDao<WalletOrder> {
 
-
+    List<WalletOrder> readTransferList(@Param("userId") String userId, @Param("list") List<Integer> list, @Param("page") Page page);
 }
--- hansen-api-service/src/main/java/com/service/WalletOrderService.java
+++ hansen-api-service/src/main/java/com/service/WalletOrderService.java
@@ -1,9 +1,13 @@
 package com.service;
 
+import com.base.page.Page;
+import com.base.page.PageResult;
 import com.base.service.CommonService;
 import com.constant.WalletOrderStatus;
 import com.constant.WalletOrderType;
 import com.model.WalletOrder;
+
+import java.util.List;
 
 /**
  * @date 2016年11月27日
@@ -14,4 +18,6 @@ public interface WalletOrderService extends CommonService<WalletOrder> {
     WalletOrder addWalletOrder(String fromUserId, String toUserId, WalletOrderType walletOrderType, Double amt, Double confirmAmt, Double poundage, WalletOrderStatus walletOrderStatus) throws Exception;
 
     Boolean coinOut(String fromUserId, String toUserId, String address, WalletOrderType walletOrderType, Double amt) throws Exception;
+
+    public PageResult<WalletOrder> readTransferList(String userId, List<Integer> orderType, Page  page) throws Exception;
 }
